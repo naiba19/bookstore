1a1210af20d94ba5f7f843237aead1ffb686ba95
Naiba <naiba.radshabov@outlook.de>
2026-09-17 14:54:30 +0200

    Added  bookstore database

--- database.js
+++ database.js
@@ -1,22 +1,193 @@
 let books = [
     {
-    "name": "Raising Mentally Strong Kids",
-    "author": "Daniel G. Amen & Charles Fay",
-    "likes": 500,
-    "liked": false,
-    "price": 29.99,
-    "publishedYear": 2024,
-    "genre": "Neuropsychology",
-    "image": "./images/raising-mentally-strong-kids",
-    "comments": [
-        {
-            "name": "TinaC",
-            "comment": "Practical, insightful and very relatable"
-        },
-        {
-            "name": "Austriantravelmummy - BCD",
-            "comment": "The book is full of relatable examples and stories, which make it a really engaging read—not too theoretical, but grounded in everyday parenting situations. I’ve already taken away quite a few ideas that I can implement straight away."
-          }
+        "name": "Raising Mentally Strong Kids",
+        "author": "Daniel G. Amen & Charles Fay",
+        "likes": 500,
+        "liked": false,
+        "price": 29.99,
+        "publishedYear": 2024,
+        "genre": "Neuropsychology",
+        "image": "./images/raising-mentally-strong-kids.webp",
+        "comments": [
+            {
+                "name": "TinaC",
+                "comment": "Practical, insightful and very relatable"
+            },
+            {
+                "name": "Austriantravelmummy - BCD",
+                "comment": "The book is full of relatable examples and stories, which make it a really engaging read—not too theoretical, but grounded in everyday parenting situations. I’ve already taken away quite a few ideas that I can implement straight away."
+            }
+        ]
+    },
+
+    {
+        "name": "You Can Heal Your Life",
+        "author": "Louise Hay",
+        "likes": 1720,
+        "liked": false,
+        "price": 16.99,
+        "publishedYear": 1984,
+        "genre": "Self-Help",
+        "image": "./images/you-can-heal-your-life.webp",
+        "comments": [
+            {
+                "name": "SarahM",
+                "comment": "No other spiritual book /self improvement book I've read connects with me the way "You can Heal Your Life" does. Louise Hay's writing is simple and to the point, not poetic, yet still beautifully put and there is a warmth to her writing which touches the soul."
+            },
+            {
+                "name": "Sharon M",
+                "comment": "This book has sold millions of copies. It gives us the essence of Louise Hay's teachings, which is that we can change our life by changing our thoughts. Negative thoughts give negative results, while positive thoughts give positive results. Millions throughout the world have changed their life for the positive and created health and abundance by following the teachings in this book."
+            },
+            {
+                "name": "Sarah",
+                "comment": "Life-changing if you are willing to change."
+            }
+        ]
+    },
+
+    {
+        "name": "Scattered Minds",
+        "author": "Gabor Maté",
+        "likes": 1120,
+        "liked": true,
+        "price": 15.00,
+        "publishedYear": 2023,
+        "genre": "Psychologie",
+        "image": "scattered-minds.webp",
+        "comments": [
+            {
+                "name": "Joshua R.",
+                "comment": "Important for ADD adults to know themselves better"
+            },
+            {
+                "name": "Matt Lanctot",
+                "comment": "Dr. Mate knows both from clinical training, experience, and his personal life the effects of ADD and how to use them to overcome life's disadvantages. This book is a deep search into the soul of the person affected by the 'disorder' which, as Dr. Mate suggests, is not a disorder but an opportunity to become oneself authentically. It's already improved my relationships with others, my child, and to myself."
+            }
+        ]
+    },
+
+
+    {
+        "name": "Becoming Supernatural",
+        "author": "Joe Dispenza",
+        "likes": 1980,
+        "liked": false,
+        "price": 21.50,
+        "publishedYear": 2019,
+        "genre": "Spirituality",
+        "image": "becoming-supernatural.jpg",
+        "comments": [
+            {
+                "name": "MindReader",
+                "comment": "Great book about the functions of human brain and how meditation affects us."
+            },
+            {
+                "name": "SoulSeeker",
+                "comment": "The best book on Earth🏔️☀️ It will make your life magnificent, by teaching you how Unlimited you are🏔️"
+            }
+        ]
+    },
+
+    {
+        "name": "The Artist's Way",
+        "author": "Julia Cameron",
+        "likes": 1340,
+        "liked": false,
+        "price": 22.99,
+        "publishedYear": 2020,
+        "genre": "Kreativität",
+        "image": "the-artist-s-way.webp",
+        "comments": [
+            {
+                "name": "CreativeSoul",
+                "comment": "This book writes about it beautifully, but also in such an easy to understand, practical way- if you are a blocked creative person, this book will change your life. I have been feeling a much deeper connection with my creativity and have a much better understanding of many things relating to art and spirituality. Highly recommend this book!"
+            },
+            {
+                "name": "LF",
+                "comment": "It’s a great book if you’re struggling with a creative rut, it has a lot of exercises you can follow and I like the way it’s written. It doesn’t use world to make it seem smarter and is very digestible. I take it with me wherever go."
+            }
+        ]
+    },
+    {
+        "name": "The Let Them Theory",
+        "author": "Mel Robbins & Sawyer Robbins",
+        "likes": 1560,
+        "liked": true,
+        "price": 29.00,
+        "publishedYear": 2024,
+        "genre": "Self-help",
+        "image": "the-let-them-theory.jpg",
+        "comments": [
+            {
+                "name": "N Chris",
+                "comment": "Absolute must read if you want to learn how to live a happy life by protecting your precious time & high energy level."
+            },
+            {
+                "name": "Rose",
+                "comment": "It’s the answer to so many questions. Let them is that book that touches the most fundamental aspects of life and how we interact and navigate our relationship. A must read book that will help you learn how to deal with friction."
+            }
+        ]
+    },
+    {
+        "name": "The 5 Love Languages",
+        "author": "Gary Chapman",
+        "likes": 14650,
+        "liked": true,
+        "price": 18.29,
+        "publishedYear": 2015,
+        "genre": "Relationships",
+        "image": "five-love-languages.webp",
+        "comments": [
+            {
+                "name": "Jessica S.",
+                "comment": "Beautiful insights to use in all types of relationships! Indeed, I wish I read this book a decade ago, when I was at the start of my dating journey."
+            },
+            {
+                "name": "Marcel Klug",
+                "comment": "This book can help your marriage if you want the help."
+            }
+        ]
+    },
+
+    {
+        "name": "The Power of Your Subconscious Mind",
+        "author": "Joseph Murphy",
+        "likes": 1780,
+        "liked": true,
+        "price": 14.99,
+        "publishedYear": 2009,
+        "genre": "Self-help",
+        "image": "the-power-of-your-subconscious-mind.webp",
+        "comments": [
+            {
+                "name": "Jack",
+                "comment": "A timeless guide to positive thinking and personal growth"
+            },
+            {
+                "name": "John David",
+                "comment": "Change your thoughts, change your life."
+            }
+        ]
+    },
+    {
+        "name": "Atomic Habits",
+        "author": "James Clear",
+        "likes": 135250,
+        "liked": true,
+        "price": 24.50,
+        "publishedYear": 2019,
+        "genre": "Self-Help",
+        "image": "atomic-habits.webp",
+        "comments": [
+            {
+                "name": "Lissi",
+                "comment": "Such a great and inspiring book! I would genuinely recommend it to anyone who wants to make positive changes in their everyday life. It’s fascinating to see how small habits can have such a powerful impact over time.The ideas are practical, easy to understand, and really make you reflect on your own routines."
+            },
+            {
+                "name": "Enrico84",
+                "comment": "A timeless classic: using the smallest habits to intentionally design your environment which will form you overtime. Still a good read and great for people that strive for experiencing self-efficacy and purposely want to design their life instead of letting it happen to them."
+            }
         ]
     }
+
 ];
