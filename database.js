64fb742153262b778e1a81c59a0f8e9070c9728b
Naiba <naiba.radshabov@outlook.de>
2026-09-17 13:34:09 +0200

    databese js

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 let books = [
-    "name": "Raising Mentally Strong Kids"
-    "author" "Daniel G. Amen & Charles Fay",
+    {
+    "name": "Raising Mentally Strong Kids",
+    "author": "Daniel G. Amen & Charles Fay",
     "likes": 500,
     "liked": false,
-    "price": 29, 99,
+    "price": 29.99,
     "publishedYear": 2024,
     "genre": "Neuropsychology",
-    "image":
+    "image": "./images/raising-mentally-strong-kids",
     "comments": [
         {
             "name": "TinaC",
@@ -15,6 +16,7 @@ let books = [
         {
             "name": "Austriantravelmummy - BCD",
             "comment": "The book is full of relatable examples and stories, which make it a really engaging read—not too theoretical, but grounded in everyday parenting situations. I’ve already taken away quite a few ideas that I can implement straight away."
-        }
-
-    ]
+          }
+        ]
+    }
+];
